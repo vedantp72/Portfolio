c21cb010718fdacc699933b18cefa210271b5250
VEDANT  SUDESH PATEL <138268574+vedantp72@users.noreply.github.com>
2026-01-24 11:22:23 +0530

    Refactor class names in About component
    Updated class names for styling consistency and layout.

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -37,12 +37,14 @@ function AboutPage() {
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.9, ease: 'easeOut' }}
             viewport={{ once: false, amount: 0.2 }}
-            className='md:w-1/2'>
-            <div className='text-lg text-gray-300 space-y-4 rounded-2xl p-2'>
+            className='py-10'>
+            <div className='text-lg text-gray-300 rounded-2xl p-2'>
+              <div className='bg-dark-300 rounded-2xl p-6 w-240'>
               <h3 className='text-2xl text-white font-semibold mb-4'>My Journey</h3>
               <p className='mb-8'>Hello! I'm <span className='text-fuchsia-500'>Vedant Patel</span>, a passionate Front-End Developer with a 2+ years of experience in creating dynamic and user-friendly web applications. </p>
               <p>With a strong foundation in HTML, CSS, and JavaScript, I have honed my skills in modern frameworks like React.js to build responsive and interactive web applications. I thrive on challenges and continuously seek to learn and grow in this ever-evolving field.</p>
               <p>When I'm not coding, I enjoy exploring new technologies, contributing to open-source projects, and indulging in my hobbies like traveling. I'm excited to connect with like-minded individuals and collaborate on projects that push the boundaries of web development.</p>
+            </div>
             </div>
           </motion.div>
         </div>
